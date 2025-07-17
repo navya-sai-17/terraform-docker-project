530527aae35ad2100a90ee78c785db2e780665c6
navya-sai-17 <navyasaidanaboina17@gmail.com>
2025-07-17 16:48:46 +0000

    second-commit

--- resource.tf
+++ resource.tf
@@ -1,4 +1,18 @@
 resource "azurerm_resource_group" "terraazure" {
-  name     = "terraazure"
-  location = "East US"
+    name     = "example-resources"
+    location = "East US"
+}
+ 
+resource "azurerm_storage_account" "saterra" {
+    name                     = "terrasa5171"
+    resource_group_name      = azurerm_resource_group.terraazure.name
+    location                 = azurerm_resource_group.terraazure.location
+    account_tier             = "Standard"
+    account_replication_type = "LRS"
+}
+ 
+resource "azurerm_storage_container" "sacontainer" {
+    name                  = "terra-container"
+    storage_account_name  = azurerm_storage_account.saterra.name
+    container_access_type = "private"
 }
